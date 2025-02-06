3444286d1060008835a7f99344bee209a7dae993
Tinno Owuor <tinnoowuor@gmail.com>
2025-02-06 11:04:43 +0300

    Update to Initial Commit

---
+++ 1-JS-Basics/Basics.js
@@ -0,0 +1,7 @@
+/*
+    You order a T-shirt for $10, socks for $8, and dinner plates for $20. Use JavaScript to calculate the total cost of your order.
+
+    Your bank account has $100, you spend $20 on lunch, $50 on dinner and earn $200 from your job. Calculate how much money you have.
+
+    Use document.body.innerHTML = ..; to make the web page blank.
+*/ 
---
+++ 2-JS-Nums&Math/Nums&Math.js
@@ -0,0 +1,12 @@
+/*
+    We'll use JavaScript to convert temperatures from Celsius to Fahrenheit. The formular is:
+        Fahrenheit = (Celsius * 9/5) + 32
+        Celsius = (Fahrenheit - 32) * 5/9
+        
+    The temperature is 25C. Calculate the temperature in Fahrenheit.(77)
+
+    The temperature is 86F. Calculate the temperature in Celsius.(30)
+
+    The temperature is -5C. Calculate the temperature in Fahrenheit.(23)
+    
+*/ 
